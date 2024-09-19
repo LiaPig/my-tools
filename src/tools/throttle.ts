2878123f723f0ab0a7723152cd1e9e2cb7076584
LiaPig <418607528@qq.com>
2024-09-19 10:55:16 +0800

    feat: Initialize lastTime to ensure immediate execution on first call

--- src/tools/throttle.ts
+++ src/tools/throttle.ts
@@ -10,7 +10,7 @@ function throttle<T extends (...args: any[]) => any>(func: T, interval: number):
     throw new Error('Interval cannot be negative.')
   }
   // 记录上一次函数执行的时间戳
-  let lastTime = 0
+  let last = 0
 
   /**
   * 返回一个新的函数，用于实现节流逻辑。
@@ -22,12 +22,13 @@ function throttle<T extends (...args: any[]) => any>(func: T, interval: number):
     const context = this
     // 记录现在的时间
     const now = new Date().getTime()
-    //
-    if (now - lastTime >= interval) {
+
+    // 如果当前时间与上一次执行时间的差值大于等于指定的时间间隔，则执行函数
+    if (now - last >= interval) {
       // 执行原始函数，并应用当前上下文和参数
       func.apply(context, args)
       // 将本次的触发时间，作为下次触发事件的参考时间
-      lastTime = now
+      last = now
     }
   }
 }
